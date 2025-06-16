fa1f0a0dc69276002c5d4fc428dc162bc9b5626f
Aman Reja <amnmolla742@gmail.com>
2025-06-16 22:13:50 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Banner from "./Components/Banner";
-import Main from "./Components/Main";
+
 import Footer from "./Components/Footer";
 import Features from "./Components/Features";
 import Card from "./Components/Card";
@@ -16,12 +16,11 @@ const App = () => {
       <Banner></Banner>
 
       <Features></Features>
-      {/* <Main></Main> */}
+
       <Skills></Skills>
       <Card></Card>
       <Swip></Swip>
       <Contactus></Contactus>
-
       <Footer></Footer>
     </>
   );
