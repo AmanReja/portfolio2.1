7bb54c9c09fe2bbed6fec249be4b47d5e0268671
Aman Reja <amnmolla742@gmail.com>
2025-07-02 23:57:47 +0530

    fix

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -14,6 +14,7 @@ import check from "../assets/images/banner/check.svg";
 import dev from "../assets/images/banner/dev.png";
 import ball from "../assets/images/banner/ball.svg";
 import bax from "../assets/images/banner/bax1.svg";
+import rob from "../assets/images/banner/rob.png";
 
 const Banner = () => {
   const [rotation, setRotation] = useState(0);
@@ -185,6 +186,46 @@ const Banner = () => {
           </div>
         </div>
       </div>
+      <section className="bg-white">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-black">
+              Awesome features designed to elevate your experience.
+            </h1>
+            <p className="max-w-2xl mb-6 font-light text-black lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+              Enjoy real-time collaboration, AI-powered tools, and personalized
+              settings that adapt to your workflow.
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-black rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            >
+              Get started
+              <svg
+                className="w-5 h-5 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </a>
+            <a
+              href="#contact"
+              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-black border rounded-lg bg-lime-300"
+            >
+              Hire Me
+            </a>
+          </div>
+          <div className=" lg:mt-0 lg:col-span-5 lg:flex">
+            <img src={rob} alt="mockup" />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
